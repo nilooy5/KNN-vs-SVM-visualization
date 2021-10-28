87b59ffeba263982f66e1a25ad21cdeb4473b4bf
Fazal Mahmud Niloy <niloooy@gmail.com>
2021-10-28 23:23:53 +0600

    gui rendered

--- main.py
+++ main.py
@@ -25,14 +25,7 @@ lbl_dataset.place(x=10, y=50)
 
 def select_dataset():
     selected = dataset_name.get()
-    if selected == 'iris':
-        output = 'Iris selected'
-    elif selected == 'breast_cancer':
-        output = 'Breast Cancer selected'
-    elif selected == 'wine':
-        output = 'Wine selected'
-    else:
-        output = "Please select a dataset"
+    output = selected + ' dataset selected'
 
     lbl_selected_dataset.config(text=output)
 
@@ -73,14 +66,62 @@ lbl_selected_dataset = tk.Label(text="",
                                 font=myFont)
 lbl_selected_dataset.place(x=10, y=100)
 
-# Add button
+# Add label
+lbl_classifier = tk.Label(text="Select a Classifier: ",
+                          fg="navy",
+                          anchor="w",
+                          width=25,
+                          height=1,
+                          font=myFont)
+lbl_classifier.place(x=10, y=150)
+
+# Add variable var and 2 radio buttons
+classifier_name = tk.StringVar(value="KNN")
+
+
+def select_classifier():
+    output = classifier_name.get() + ' classifier selected'
+    lbl_selected_classifier.config(text=output)
+
+
+rb_knn = tk.Radiobutton(text="K-Nearest Neighbor",
+                        variable=classifier_name,
+                        value='KNN',
+                        font=myFont,
+                        command=select_classifier)
+rb_knn.place(x=180, y=150)
+
+rb_svc = tk.Radiobutton(text="Support Vector Classification",
+                        variable=classifier_name,
+                        value='svc',
+                        font=myFont,
+                        command=select_classifier)
+rb_svc.place(x=350, y=150)
+
+# Label to display output when button is clicked
+lbl_selected_classifier = tk.Label(text="",
+                                   fg="navy",
+                                   width=25,
+                                   height=1,
+                                   font=myFont)
+lbl_selected_classifier.place(x=10, y=200)
+
+
+def select_widget_values():
+    select_dataset()
+    select_classifier()
+    print(classifier_name.get(), dataset_name.get())
+
+
+# Run button
 button = tk.Button(text="Run Model",
                    fg="black",
                    bg="lightblue",
                    width=10,
                    height=1,
                    font=myFont,
-                   command=select_dataset)
-button.place(x=10, y=150)
+                   command=select_widget_values)
+
+button.place(x=10, y=300)
 
 window.mainloop()
