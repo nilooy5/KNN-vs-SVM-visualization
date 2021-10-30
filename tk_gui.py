f3e02d8bbf29875b915a68471cdc728b94b64155
Fazal Mahmud Niloy <niloooy@gmail.com>
2021-10-31 03:03:09 +0600

    gui documentation as comments given infile

--- tk_gui.py
+++ tk_gui.py
@@ -1,26 +1,28 @@
 import tkinter as tk
 import training as training
 
-# Set font
+# some font configurations for GUI
 myFont = "Arial, 12"
 myFont_big = "Arial, 15 bold"
 myFont_medium = "Arial, 10"
 myFont_small = "Arial, 8"
 
 
+# main method that contains the tkinter widgets and methods
 def main_gui():
     window = tk.Tk()
     window.title('Programming for Data Science')
     window.geometry("700x500+400+100")
 
-    # Add a label
+    # a label for the header
     lbl_header = tk.Label(
-        text="Assignment 2",
-        font=myFont,
+        text="Assignment 2 by Fazal Mahmud Niloy (u3228358)",
+        font=myFont_big,
         height=1
     )
-    lbl_header.place(x=150, y=10)
-    # Add label
+    lbl_header.place(x=150, y=10)   # placement of header label
+
+    # a label for prompting to select dataset
     lbl_dataset = tk.Label(
         text="Select a dataset: ",
         fg="navy",
@@ -29,9 +31,9 @@ def main_gui():
         height=1,
         font=myFont
     )
-    lbl_dataset.place(x=10, y=50)
+    lbl_dataset.place(x=10, y=50)   # placement of the previous label
 
-    # Label to display output when button is clicked
+    # label to display which dataset is selected
     lbl_selected_dataset = tk.Label(
         text="",
         fg="red",
@@ -42,15 +44,17 @@ def main_gui():
     )
     lbl_selected_dataset.place(x=10, y=70)
 
+    # this method gets the string value from dataset_name and assigns it in lbl_selected_dataset label
     def select_dataset():
         selected = dataset_name.get()
         selected_dataset_string = selected + ' dataset selected'
 
-        lbl_selected_dataset.config(text=selected_dataset_string)
+        lbl_selected_dataset.config(text=selected_dataset_string)   # assigning the selected dataset value to this label
 
     # Add variable var and 2 radio buttons
-    dataset_name = tk.StringVar(value="iris")
+    dataset_name = tk.StringVar(value="iris")   # a string value for the selected dataset initially set to 'iris'
 
+    # radio button configuration for iris dataset
     rb_iris = tk.Radiobutton(
         text="Iris",
         variable=dataset_name,
@@ -60,6 +64,7 @@ def main_gui():
     )
     rb_iris.place(x=180, y=50)
 
+    # radio button configuration for breast_cancer dataset
     rb_breast_cancer = tk.Radiobutton(
         text="Breast Cancer",
         variable=dataset_name,
@@ -69,6 +74,7 @@ def main_gui():
     )
     rb_breast_cancer.place(x=250, y=50)
 
+    # radio button configuration for wine dataset
     rb_wine = tk.Radiobutton(
         text="Wine",
         variable=dataset_name,
@@ -78,7 +84,7 @@ def main_gui():
     )
     rb_wine.place(x=450, y=50)
 
-    # Add label
+    # A label to prompt the user to select a classifier
     lbl_classifier = tk.Label(
         text="Select a Classifier: ",
         fg="navy",
@@ -89,7 +95,7 @@ def main_gui():
     )
     lbl_classifier.place(x=10, y=120)
 
-    # Label to display output when button is clicked
+    # Label to display which classifier is currently selected
     lbl_selected_classifier = tk.Label(
         text="",
         fg="red",
@@ -99,13 +105,15 @@ def main_gui():
     )
     lbl_selected_classifier.place(x=10, y=140)
 
-    # Add variable var and 2 radio buttons
+    # a string variable value for the radio buttons of classifiers initial value set to KNN
     classifier_name = tk.StringVar(value="KNN")
 
+    # this method gets the value from classifier_name stringVar and assigns it to lbl_selected_classifier
     def select_classifier():
         output = classifier_name.get() + ' classifier selected'
         lbl_selected_classifier.config(text=output)
 
+    # radio button for knn classification
     rb_knn = tk.Radiobutton(
         text="K-Nearest Neighbor",
         variable=classifier_name,
@@ -115,6 +123,7 @@ def main_gui():
     )
     rb_knn.place(x=180, y=120)
 
+    # radio button for support vector classification
     rb_svc = tk.Radiobutton(
         text="Support Vector Classification",
         variable=classifier_name,
@@ -124,7 +133,7 @@ def main_gui():
     )
     rb_svc.place(x=350, y=120)
 
-    # Label to display output when button is clicked
+    # label to display the best parameter for a selected dataset and classifier technique
     lbl_best_param = tk.Label(
         text="",
         fg="navy",
@@ -134,7 +143,7 @@ def main_gui():
     )
     lbl_best_param.place(x=10, y=350)
 
-    # Label to display output when button is clicked
+    # label to display the accuracy for a selected dataset and classifier technique
     lbl_accuracy = tk.Label(
         text="",
         fg="navy",
@@ -144,16 +153,26 @@ def main_gui():
     )
     lbl_accuracy.place(x=10, y=380)
 
+    # setting initial fold_value string variable value for TK Entry Box as 5
     fold_value = tk.StringVar(value="5")
 
+    # a label that prompts the user that the entry box is for k-fold numbers
     lbl_folds = tk.Label(window, text="K-folds", font=myFont, fg="navy")
     lbl_folds.place(x=10, y=200)
+
+    # an entry box for inputting number of k-folds
     entry_fold = tk.Entry(window, bd=5, textvariable=fold_value)
     entry_fold.place(x=180, y=200)
 
+    # an additional label that pops a message if there is an error entering the fold number values
     lbl_error = tk.Label(window, text="", font=myFont_small, fg="red")
     lbl_error.place(x=180, y=220)
 
+    # this method is triggered when the run model button is pressed
+    # this will show and handle error if anything other than number that are bigger than 1 is inputted
+    # this will also pass the necessary data and configurations such as
+    # dataset name, classification name, number of folds, object of best parameter label and object of accuracy to
+    # run_classification() of training.py module
     def select_widget_values():
         lbl_error.config(text="")
         try:
@@ -164,14 +183,13 @@ def main_gui():
                 dataset_name=dataset_name.get(),
                 classification_name=classifier_name.get(),
                 number_of_folds=folds,
-                root=window,
                 label_best_parameter=lbl_best_param,
                 label_accuracy=lbl_accuracy
             )
         except ValueError:
             lbl_error.config(text='put a valid number > 1')
 
-    # Run button
+    # Run button configuration. it runs the select_widget_values method
     button = tk.Button(
         text="Run Model",
         fg="black",
