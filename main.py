0015563c32212858e54ff1b2dd5910a4a28fda86
Fazal Mahmud Niloy <niloooy@gmail.com>
2021-10-31 03:52:18 +0600

    name and details with comments added

--- main.py
+++ main.py
@@ -1,3 +1,13 @@
+#########################################
+# Assignment 2                          #
+# name:   Fazal Mahmud Niloy            #
+# ID:     3228358                       #
+# email:  u3228358@uni.canberra.edu.au  #
+# Unit:   Programming for Data Science  #
+# Course: Master of Data Science        #
+#########################################
+
 from tk_gui import main_gui
 
+# this method only runs the main gui where everything is contained
 main_gui()
