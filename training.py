fe46ed3c5c78edc0390141674a083a94991595b7
Fazal Mahmud Niloy <niloooy@gmail.com>
2021-10-31 03:04:09 +0600

    code formatted. unnecessary imports removed

--- training.py
+++ training.py
@@ -1,15 +1,9 @@
-import tkinter
-
-import numpy as np
-from matplotlib.backends._backend_tk import NavigationToolbar2Tk
-from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
-from matplotlib.figure import Figure
-from sklearn import datasets, neighbors, metrics, mixture, svm
+from sklearn import datasets, neighbors, metrics, svm
 from sklearn.model_selection import train_test_split, GridSearchCV
 import matplotlib.pyplot as plt
 
 
-def run_classification(dataset_name, classification_name, number_of_folds, root, label_best_parameter, label_accuracy):
+def run_classification(dataset_name, classification_name, number_of_folds, label_best_parameter, label_accuracy):
     if dataset_name == 'iris':
         dataset = datasets.load_iris()
     elif dataset_name == 'breast_cancer':
@@ -20,9 +14,11 @@ def run_classification(dataset_name, classification_name, number_of_folds, root,
     if classification_name == 'KNN':
         classifier = neighbors.KNeighborsClassifier()
         parameter = [{'n_neighbors': range(30)}]
+        x_label = 'Value of K for KNN'
     elif classification_name == 'svc':
         classifier = svm.SVC()
         parameter = [{'gamma': [0.0001, 0.001, 0.01, 0.1, 1.0]}]
+        x_label = 'Parameter C'
 
     x = dataset.data
     y = dataset.target
@@ -54,44 +50,24 @@ def run_classification(dataset_name, classification_name, number_of_folds, root,
              + str(gscv_classifier.best_params_)
              + ' score: {0:.2f}%'.format(gscv_classifier.best_score_)
     )
-    print("Best parameter:" + str(gscv_classifier.best_params_) + ' accuracy: {0:.2f}%'.format(gscv_classifier.best_score_))
+    print("Best parameter:"
+          + str(gscv_classifier.best_params_)
+          + ' & accuracy score: {0:.2f}%'.format(gscv_classifier.best_score_))
 
     y_pred = gscv_classifier.predict(X_test)
     # • Plot confusion matrix and accuracy
     accuracy = metrics.accuracy_score(Y_test, y_pred) * 100
+    print('overall accuracy: {0:.2f}%'.format(accuracy))
     label_best_parameter.config(text='Accuracy = {0:.2f}%'.format(accuracy))
     plotcm = metrics.ConfusionMatrixDisplay.from_estimator(gscv_classifier, X_test, Y_test, display_labels=class_names)
     plotcm.ax_.set_title('Accuracy = {0:.2f}%'.format(accuracy))
     plt.show()
 
-    # fig2 = Figure(figsize=(3, 3), dpi=100)
-    # t2 = np.arange(0, 3, .01)
-    # fig2.add_subplot(111).plot(y_pred)
-    #
-    # canvas2 = FigureCanvasTkAgg(fig2, master=root)  # A tk.DrawingArea.
-    # canvas2.draw()
-    # canvas2.get_tk_widget().pack(side=tkinter.TOP, expand=0)
-
-    # toolbar2 = NavigationToolbar2Tk(canvas2, root)
-    # toolbar2.update()
-    # canvas2.get_tk_widget().pack(side=tkinter.TOP, expand=0)
-
-    #   TODO: plot axis names
-    #   TODO: try embedding plots in tkinter
-    #   TODO: remove unnecessary module imports
-
     x_axis = list(parameter[0].values())[0]
     y_axis = means
-    line_style = ''
     plot_axes = plt.axes()
-    plot_axes.plot(x_axis, y_axis, line_style)
+    plt.xlabel(x_label)
+    plt.ylabel('CV score')
+    plot_axes.plot(x_axis, y_axis)
     plt.show()
-
-    # fig3 = Figure(figsize=(3, 3), dpi=100)
-    # t3 = np.arange(0, 3, .01)
-    # fig3.add_subplot(111).plot(x_axis, y_axis)
-    #
-    # canvas3 = FigureCanvasTkAgg(fig3, master=root)  # A tk.DrawingArea.
-    # canvas3.draw()
-    # canvas3.get_tk_widget().pack(side=tkinter.TOP, expand=0)
 
